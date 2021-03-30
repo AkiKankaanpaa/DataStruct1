cefa00a518752a412c187bd154559b29130d8262
Kiriela <aki.koulu@gmail.com>
2021-03-30 19:11:30 +0300

    Added comments to the datastructures class, made some functions be more consistent with their usage of private functions.

--- prg1/datastructures.cc
+++ prg1/datastructures.cc
@@ -17,11 +17,6 @@ Type random_in_range(Type start, Type end)
     return static_cast<Type>(start+num);
 }
 
-// Modify the code below to implement the functionality of the class.
-// Also remove comments from the parameter names when you implement
-// an operation (Commenting out parameter name prevents compiler from
-// warning about unused parameters on operations you haven't yet implemented.)
-
 Datastructures::Datastructures():
     coordinate_sorted_(false),
     alphabetical_sorted_(false),
@@ -31,12 +26,10 @@ Datastructures::Datastructures():
     places_by_name_({}),
     places_by_type_({})
 {
-    // Replace this comment with your implementation
 }
 
 Datastructures::~Datastructures()
 {
-    // Replace this comment with your implementation
 }
 
 int Datastructures::place_count()
@@ -76,6 +69,7 @@ bool Datastructures::add_place(PlaceID id, const Name& name, PlaceType type, Coo
     places_by_name_.insert({name, added_place});
     places_by_type_.insert({type, added_place});
 
+    // Since adding a value changes all Place-relevant datastructures, raise both flags
     coordinate_sorted_ = false;
     alphabetical_sorted_ = false;
     return true;
@@ -102,7 +96,6 @@ Coord Datastructures::get_place_coord(PlaceID id)
 bool Datastructures::add_area(AreaID id, const Name &name, std::vector<Coord> coords)
 {
     auto found_area = get_area(id);
-    // Making sure that no area already exists with the same AreaID
     if (found_area != nullptr) {
         return false;
     }
@@ -140,9 +133,11 @@ std::vector<PlaceID> Datastructures::places_alphabetically()
     if (!alphabetical_sorted_) {
         alphabetical_vector_ids_ = {};
         std::multimap<Name, std::shared_ptr<Place>> temp_map = {};
+        // First pushing to temp_map which automatically becomes sorted
         for (auto it = places_by_id_.begin(); it != places_by_id_.end(); ++it) {
             temp_map.insert({it->second->name, it->second });
         }
+        // Then simply pushing the ids in order to the vector
         for(auto it = temp_map.begin(); it != temp_map.end(); ++it) {
             alphabetical_vector_ids_.push_back(it->second->id);
         }
@@ -156,9 +151,11 @@ std::vector<PlaceID> Datastructures::places_coord_order()
     if (!coordinate_sorted_) {
         coordinate_vector_ids_ = {};
         std::multimap<Coord, std::shared_ptr<Place>> temp_map = {};
+        // First pushing to temp_map which automatically becomes sorted
         for (auto it = places_by_id_.begin(); it != places_by_id_.end(); ++it) {
             temp_map.insert({it->second->coordinates, it->second });
         }
+        // Then simply pushing the ids in order to the vector
         for(auto it = temp_map.begin(); it != temp_map.end(); ++it) {
             coordinate_vector_ids_.push_back(it->second->id);
         }
@@ -203,6 +200,7 @@ bool Datastructures::change_place_name(PlaceID id, const Name& newname)
             auto wanted_element = places_by_name_.extract(it);
             wanted_element.key() = newname;
             places_by_name_.insert(std::move(wanted_element));
+            break;
         }
     }
     alphabetical_sorted_ = false;
@@ -211,11 +209,12 @@ bool Datastructures::change_place_name(PlaceID id, const Name& newname)
 
 bool Datastructures::change_place_coord(PlaceID id, Coord newcoord)
 {
-    auto found_value = places_by_id_.find(id);
-    if (found_value == places_by_id_.end()) {
+    auto found_place = get_place(id);
+    if (found_place == nullptr) {
         return false;
     }
-    found_value->second->coordinates = newcoord;
+
+    found_place->coordinates = newcoord;
     coordinate_sorted_ = false;
     return true;
 }
@@ -231,18 +230,18 @@ std::vector<AreaID> Datastructures::all_areas()
 
 bool Datastructures::add_subarea_to_area(AreaID id, AreaID parentid)
 {
-    auto found_subarea = areas_by_id_.find(id);
-    auto found_parent_area = areas_by_id_.find(parentid);
-    auto end_of_areas = areas_by_id_.end();
-    if (found_subarea == end_of_areas || found_parent_area == end_of_areas) {
+    auto found_subarea = get_area(id);
+    auto found_parent_area = get_area(parentid);
+    if (found_subarea == nullptr || found_parent_area == nullptr) {
         return false;
     }
-    if (found_subarea->second->parent_area != nullptr) {
+    // Done separately to prevent segmentation fault if found_subarea points to a nullptr
+    if (found_subarea->parent_area != nullptr) {
         return false;
     }
 
-    found_subarea->second->parent_area = found_parent_area->second;
-    found_parent_area->second->subareas.push_back(found_subarea->second);
+    found_subarea->parent_area = found_parent_area;
+    found_parent_area->subareas.push_back(found_subarea);
     return true;
 }
 
@@ -253,12 +252,13 @@ std::vector<AreaID> Datastructures::subarea_in_areas(AreaID id)
         return {NO_AREA};
     }
 
-    std::vector<AreaID> higher_areas = {};
+    std::vector<AreaID> parents = {};
+    // Going through all of the parents and adding them to parents vector
     while (found_area->parent_area != nullptr) {
         found_area = found_area->parent_area;
-        higher_areas.push_back(found_area->id);
+        parents.push_back(found_area->id);
     }
-    return higher_areas;
+    return parents;
 }
 
 std::vector<PlaceID> Datastructures::places_closest_to(Coord xy, PlaceType type)
@@ -320,21 +320,25 @@ bool Datastructures::remove_place(PlaceID id)
     if (to_be_removed == nullptr) {
         return false;
     }
+
+    // Only iterating over a specific key in the multimaps
+    auto name_range = places_by_name_.equal_range(to_be_removed->name);
+    for (auto name_iter = name_range.first; name_iter != name_range.second; name_iter++) {
+        if (name_iter->second->id == id) {
+            places_by_name_.erase(name_iter);
+            break;
+        }
+    }
+
+    auto type_range = places_by_type_.equal_range(to_be_removed->type);
+    for (auto type_iter = type_range.first; type_iter != type_range.second; type_iter++) {
+        if (type_iter->second->id == id) {
+            places_by_type_.erase(type_iter);
+            break;
+        }
+    }
+
     places_by_id_.erase(id);
-
-    for (auto name_it = places_by_name_.begin(); name_it != places_by_name_.end(); name_it++) {
-        if (name_it->second->id == id) {
-            places_by_name_.erase(name_it);
-            break;
-        }
-    }
-
-    for (auto type_it = places_by_type_.begin(); type_it != places_by_type_.end(); type_it++) {
-        if (type_it->second->id == id) {
-            places_by_type_.erase(type_it);
-            break;
-        }
-    }
     coordinate_sorted_ = false;
     alphabetical_sorted_ = false;
     return true;
@@ -351,17 +355,18 @@ std::vector<AreaID> Datastructures::all_subareas_in_area(AreaID id)
 
 AreaID Datastructures::common_area_of_subareas(AreaID id1, AreaID id2)
 {
-    auto first_area = areas_by_id_.find(id1);
-    auto second_area = areas_by_id_.find(id2);
-    if (first_area == areas_by_id_.end() || second_area == areas_by_id_.end()) {
+    auto first_area = get_area(id1);
+    auto second_area = get_area(id2);
+    if (first_area == nullptr || second_area == nullptr) {
         return NO_AREA;
     }
 
-    auto first_parent_pointer = first_area->second->parent_area;
-    auto second_parent_pointer = second_area->second->parent_area;
+    auto first_parent_pointer = first_area->parent_area;
+    auto second_parent_pointer = second_area->parent_area;
     std::vector<std::shared_ptr<Area>> first_parents;
     std::vector<std::shared_ptr<Area>> second_parents;
 
+    // Pushing all of the parents one by one into the vectors first_parents and second_parents
     while (first_parent_pointer != nullptr) {
         first_parents.push_back(first_parent_pointer);
         first_parent_pointer = first_parent_pointer->parent_area;
@@ -372,12 +377,15 @@ AreaID Datastructures::common_area_of_subareas(AreaID id1, AreaID id2)
         second_parent_pointer = second_parent_pointer->parent_area;
     }
 
+    // Check for the first value that differs between the parents, starting from the root
     auto result = std::mismatch(first_parents.rbegin(), first_parents.rend(),
                                 second_parents.rbegin(), second_parents.rend());
 
+    // If the first value is different, the two areas have a different root and by so do not have a common area
     if (result.first == first_parents.rbegin()) {
         return NO_AREA;
     }
+    // Change to the common parent instead of the first differing value by going back by one
     --result.first;
     return result.first->get()->id;
 }
@@ -390,7 +398,7 @@ std::shared_ptr<Place> Datastructures::get_place(PlaceID id) {
     return search_by_id->second;
 }
 
-std::shared_ptr<Area> Datastructures::get_area(PlaceID id)
+std::shared_ptr<Area> Datastructures::get_area(AreaID id)
 {
     auto search_by_id = areas_by_id_.find(id);
     if (search_by_id == areas_by_id_.end()) {
@@ -399,11 +407,14 @@ std::shared_ptr<Area> Datastructures::get_area(PlaceID id)
     return search_by_id->second;
 }
 
+// This function is called recursively to find out all of the children
 std::vector<AreaID> Datastructures::get_children(std::weak_ptr<Area> current_area)
 {
     std::vector<AreaID> subareas = {};
     for (auto child: current_area.lock()->subareas) {
+        // Add the child to the subareas vector
         subareas.push_back(child.lock()->id);
+        // And then return all of the subareas of the child and push them into subareas vector
         auto children_subareas = get_children(child);
         for (auto child: children_subareas) {
             subareas.push_back(child);
